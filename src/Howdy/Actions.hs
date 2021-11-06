197d446a0c0f11c56591579afcf85c6e522f7755
kam <11618271+stereoknife@users.noreply.github.com>
2021-11-06 19:55:39 +0200

    clean up actions and bot

--- src/Howdy/Actions.hs
+++ src/Howdy/Actions.hs
@@ -1,14 +1,58 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Howdy.Actions where
 
-import Data.Text (Text)
-import Discord (DiscordHandler)
-import Control.Monad.State (StateT)
+import           Control.Monad.State  (StateT)
+import           Control.Monad.Writer (MonadWriter (tell), Writer, execWriter)
+import           Data.Default         (Default (def))
+import           Data.Semigroup       (Semigroup)
+import           Data.Text            (Text)
+import           Discord              (DiscordHandler)
 
 -- Command context has message/parser as reader (state but it don't matter) around a DiscordHandler output
 
 newtype ActionContext a = ActionContext { runAction :: StateT Text DiscordHandler a }
+    deriving (Functor, Applicative, Monad)
 
-data Command = Command { alias :: Text
-                       , desc  :: Text
-                       , action :: ActionContext ()
-                       }
+-- There should be some data structure defining parsed data ??
+
+data CommandData = CommandData { getAlias       :: [Text]
+                               , getDesc        :: Text
+                               , getAction      :: ActionContext ()
+                               , getSubcommands :: [CommandData]
+                               }
+
+instance Semigroup CommandData where
+    a <> b = CommandData
+             (getAlias a <> getAlias b)
+             (getDesc a <> getDesc b)
+             (getAction a >> getAction b)
+             (getSubcommands a <> getSubcommands b)
+
+instance Monoid CommandData where
+    mempty = CommandData mempty mempty (pure ()) mempty
+
+newtype CommandBuilder a = CommandBuilder { run_CB :: Writer CommandData a}
+    deriving (Functor, Applicative, Monad, MonadWriter CommandData)
+
+--newtype BotBuilder a = BotBuilder {run_BB :: Writer BotData a}
+--    deriving (Functor, Applicative, Monad, MonadWriter BotData)
+
+--command :: CommandBuilder () -> BotBuilder ()
+--command c = tell $ mempty{commands = [execWriter $ run_CB c]}
+
+alias :: Text -> CommandBuilder ()
+alias a = tell $ mempty{getAlias = [a]}
+
+aliases :: [Text] -> CommandBuilder ()
+aliases a = tell $ mempty{getAlias = a}
+
+desc :: Text -> CommandBuilder ()
+desc d = tell $ mempty{getDesc = d}
+
+action :: ActionContext () -> CommandBuilder ()
+action a = tell $ mempty{getAction = a}
+
+subcommand :: CommandBuilder () -> CommandBuilder ()
+subcommand s = tell $ mempty{getSubcommands = [execWriter $ run_CB s]}
+
--- src/Howdy/Bot.hs
+++ src/Howdy/Bot.hs
@@ -1,115 +1,46 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE KindSignatures #-}
 
 module Howdy.Bot where
 
-import           Control.Applicative    ( Alternative(empty, (<|>)) )
-import           Control.Monad          ( guard )
-import           Control.Monad.IO.Class ( MonadIO(liftIO) )
-import           Control.Monad.Reader   ( ReaderT(runReaderT), void, when )
-import           Control.Monad.Writer
-    ( MonadWriter(tell), Writer, WriterT(WriterT), execWriter, when )
+import           Control.Monad.Writer (MonadWriter (tell), Writer,
+                                       WriterT (WriterT), execWriter)
+import           Data.Text            (Text)
+import           Howdy.Actions        (CommandBuilder (run_CB), CommandData)
 
-import           Data.Default           ( Default(..) )
-import qualified Data.HashMap.Strict    as M
-import qualified Data.HashSet           as S
-import           Data.Maybe             ( fromMaybe )
-import           Data.Text              ( Text )
-import qualified Data.Text              as T
-import qualified Data.Text.IO           as TIO
 
-import           Discord
-    ( DiscordHandler
-    , RunDiscordOpts(discordToken, discordOnEvent, discordOnStart,
-               discordOnEnd, discordOnLog), def
-    , runDiscord )
-import           Discord.Types
-    ( Emoji, Event(MessageCreate), Message(messageText) )
 
-import           Howdy.Parser           ( firstof, runParser, string, word )
+--type Commands = M.HashMap Text ()
 
-import           System.Environment     ( getEnv )
+--type Reactions = M.HashMap Emoji ()
 
-type Commands = M.HashMap Text ()
+--type Prefixes = S.HashSet Text
 
-type Reactions = M.HashMap Emoji ()
 
-type Prefixes = S.HashSet Text
-
-{-}
-
-data BotData = BotData { prefixes :: [Text]
-                       , commands :: Commands
-                       , reactions :: Reactions
+data BotData = BotData { getPrefixes  :: [Text]
+                       , getCommands  :: [CommandData]
+                       , getReactions :: [()]
                        }
 
-instance Default BotData where
-    def = BotData mempty mempty mempty
-
 instance Semigroup BotData where
-    a <> b = BotData { prefixes  = prefixes a <> prefixes b
-                     , commands  = commands a <> commands b
-                     , reactions = reactions a <> reactions b
+    a <> b = BotData { getPrefixes  = getPrefixes a <> getPrefixes b
+                     , getCommands  = getCommands a <> getCommands b
+                     , getReactions = getReactions a <> getReactions b
                      }
 
 instance Monoid BotData where
-    mempty = def
+    mempty = BotData mempty mempty mempty
 
-newtype Bot a = MkBot (Writer BotData a)
-    deriving ( Functor, Applicative, Monad, MonadWriter BotData )
+newtype BotBuilder a = BotBuilder {run_BB :: Writer BotData a}
+    deriving (Functor, Applicative, Monad, MonadWriter BotData)
 
-getBot :: Bot a -> BotData
-getBot (MkBot w) = execWriter w
+command :: CommandBuilder () -> BotBuilder ()
+command c = tell $ mempty{getCommands = [execWriter $ run_CB c]}
 
-prefix :: [ Text ] -> Bot ()
-prefix pf = tell $ def { prefixes = pf }
+prefix :: Text -> BotBuilder ()
+prefix p = tell $ mempty{getPrefixes = [p]}
 
-command :: Text -> Text -> DiscordHandler () -> Bot ()
-command alias desc action
-    = tell $ def { commands = M.singleton alias $ Command alias desc action }
+prefixes :: [Text] -> BotBuilder ()
+prefixes p = tell $ mempty{getPrefixes = p}
 
-reaction :: Emoji -> Text -> DiscordHandler () -> Bot ()
-reaction emoji desc action
-    = tell $ def { reactions = M.singleton emoji $ Reaction emoji desc action }
-
-eventHandler :: BotData -> Event -> DiscordHandler ()
-eventHandler bot (MessageCreate m) = go $ do
-    firstof string $ prefixes bot
-    alias <- word
-    pure $ runCommands alias $ commands bot
-  where
-    parsed p = runParser p $ messageText m
-
-    go p' = seq (parsed p') (pure ())
-eventHandler _ _ = pure ()
-
-run :: Bot () -> IO ()
-run b' = do
-    tok <- TIO.readFile "./token.secret" <|> T.pack <$> getEnv "DISCORD_TOKEN"
-    let b = getBot b'
-    -- open ghci and run  [[ :info RunDiscordOpts ]] to see available fields
-    t <- runDiscord $ def
-        { discordToken   = tok
-        , discordOnStart = startHandler
-        , discordOnEnd   = liftIO $ putStrLn "Ended"
-        , discordOnEvent = eventHandler b
-        , discordOnLog   = \s -> TIO.putStrLn s >> TIO.putStrLn ""
-        }
-    TIO.putStrLn t
-
-startHandler :: DiscordHandler ()
-startHandler = error "not implemented"
-
-
--- TODO: Make action mcmonad so it can have the nice cool data it needs
-runCommand :: Text -> Command -> Maybe (DiscordHandler ())
-runCommand t (Command a _ c) = guard (t == a) >> Just c
-
-runCommands :: Text -> [ Command ] -> DiscordHandler ()
-runCommands t = fromMaybe (pure ()) . go
-  where
-    go = foldr ((<|>) . runCommand t) empty
-
--}
+reaction :: () -> BotBuilder ()
+reaction r = tell $ mempty{getReactions = [r]}
